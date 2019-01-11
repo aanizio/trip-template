a1351d69b4c65187572bc0b24baccf0871f0d01a
Anderson Anizio <anderson.anizio@gmail.com>
2019-01-11 13:29:56 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Trip Directory Listing Template
+
+Template created to practice web development.
+
+## Objectives
+
+Technologies and features I use on this template:
+- BEM CSS
+- Pug Template Engine
+- CSS Flexbox and Grid Layout
+- SASS
+
+## Install
+
+Run the following command to install all the dependencies:
+
+    npm install
+
+## Run
+
+To just build distribuition folder, use default gulp task:
+
+    gulp
+
+To run a local server:
+
+    gulp serve
+
+To serve and watch for changes:
+
+    gulp watch
+
+## Credits
+
+- Photos on [Unsplash](https://unsplash.com/) by:
+  - [Oliver Ash](https://unsplash.com/@oliverjash)
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const { src, dest, series, watch } = require('gulp'),
+  pug = require('gulp-pug'),
+  pugLocals = require('./src/_data/site.json'),
+  clean = require('gulp-clean'),
+  bSync = require('browser-sync').create(),
+  sass = require('gulp-sass'),
+  sassGlob = require('gulp-sass-glob'),
+  postcss = require('gulp-postcss'),
+  cssnano = require('gulp-cssnano'),
+  rename = require('gulp-rename'),
+  autoprefixer = require('autoprefixer');
+
+function cleanDistDir() {
+  return src('dist/', {read: false, allowEmpty: true})
+    .pipe(clean());
+}
+
+function browserSync() {
+  bSync.init({
+    server: {
+      baseDir: 'dist/',
+    },
+  })
+}
+
+function buildHTML() {
+  return src('src/pug/pages/*.pug')
+    .pipe(pug({
+      locals: pugLocals,
+      pretty: true,
+    }))
+    .pipe(dest('dist/'))
+    .pipe(bSync.stream());
+}
+
+function buildCSS() {
+  return src('src/sass/main.scss')
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css'))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(cssnano())
+    .pipe(rename(path => {
+      path.extname = '.min.css'
+    }))
+    .pipe(dest('dist/css'))
+    .pipe(bSync.stream());
+}
+
+function buildImages() {
+  return src('src/images/*')
+    .pipe(dest('dist/images'));
+}
+
+const buildSite = series(cleanDistDir, buildHTML, buildCSS, buildImages);
+
+function serveAndWatch() {
+  browserSync();
+  watch('src/**/*', buildSite);
+}
+
+exports.default = buildSite;
+exports.serve = browserSync;
+exports.watch = serveAndWatch;
