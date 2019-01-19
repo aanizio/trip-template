06e8e51031632650caf139fa0875e02ed400d77d
Anderson Anizio <anderson.anizio@gmail.com>
2019-01-19 15:33:37 -0200

    Footer added

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,4 +2,5 @@ module.exports = {
     siteTitle: "Trip template",
     siteLangCode: "en",
     siteDescription: "A HTML template for trip purposes",
+    copyrightYear: (new Date).getFullYear(),
 }
